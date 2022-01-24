6ae9d8fb1c7261684d040340b98204d6af4428a7
kunal valecha <kunalvalecha1@gmail.com>
2022-01-24 16:59:05 +0530

    add serilog to Shopping aggregator, Catalog

--- src/ApiGateways/Shopping.Aggregator/Startup.cs
+++ src/ApiGateways/Shopping.Aggregator/Startup.cs
@@ -1,3 +1,4 @@
+using Common.Logging;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -26,24 +27,27 @@ namespace Shopping.Aggregator
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Shopping.Aggregator", Version = "v1" });
             });
+
+            services.AddTransient<LoggingDelegatingHandler>();
+
             services.AddHttpClient<ICatalogService, CatalogService>(client=>
             {
                 client.BaseAddress = new Uri(Configuration["ApiSettings:CatalogUrl"]);
-                });
+                }).AddHttpMessageHandler<LoggingDelegatingHandler>();
             services.AddHttpClient<IBasketService, BasketService>(client =>
             {
                 client.BaseAddress = new Uri(Configuration["ApiSettings:BasketUrl"]);
-            });
+            }).AddHttpMessageHandler<LoggingDelegatingHandler>();
             services.AddHttpClient<IOrderService, OrderService>(client =>
             {
                 client.BaseAddress = new Uri(Configuration["ApiSettings:OrderingUrl"]);
-            });
+            }).AddHttpMessageHandler<LoggingDelegatingHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
